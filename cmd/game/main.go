6696d2df052cba9cd6d8f3084e29259f0d8feb22
Orzzet <adrian.lc92@gmail.com>
2022-05-01 10:56:49 +0200

    feat: add badger storage

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"resourcesAndFactories/pkg/storage/badger"
+)
+
+func main() {
+	db, err := badger.New()
+	if err != nil {
+		fmt.Println("couldn't initialize database")
+		fmt.Println(err.Error())
+		return
+	}
+	defer db.Close()
+}
--- pkg/storage/badger/storage.go
+++ pkg/storage/badger/storage.go
@@ -1,1 +1,13 @@
 package badger
+
+import (
+	"github.com/dgraph-io/badger/v3"
+)
+
+func New() (*badger.DB, error) {
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
